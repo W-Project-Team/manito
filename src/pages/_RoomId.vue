494ae94b50cdc429691dbd9b3c269837637fe5f8
mobssie <mobssie.jung@gmail.com>
2021-12-15 00:13:51 +0900

    fix room data

--- src/pages/_RoomId.vue
+++ src/pages/_RoomId.vue
@@ -1,6 +1,7 @@
 <template>
   <transition name="fade">
-    <div class="text-center text-xl font-bold" v-if="!loading">
+    <div class="wrap_room text-center text-xl font-bold" v-if="!loading">
+      <span class="tit_paticipants">참가인원</span>
       <template v-if="currentRoom">
         <h2 class="text-gray-700">
           {{ currentRoom.title }}
@@ -17,7 +18,11 @@
         </List>
       </template>
       <template>
-
+        <div class="img_box mt-6">
+          <!-- 하드코딩임 -->
+          <span class="txt_matching">currentRoom.participants[0].name</span>
+        </div>
+        <Button class="btn-success mt-6">마니또 매칭 시작</Button>
       </template>
     </div>
   </transition>
@@ -64,3 +69,37 @@ const route = useRoute()
 const roomId = computed(() => route.params.roomId)
 
 </script>
+<style scoped lang="scss">
+.wrap_room{
+  min-width: 320px
+}
+.tit_paticipants{
+  display: inline-block;
+  margin-bottom: 10px;
+  padding: 18px 0 15px;
+  border-bottom: 3px solid #333;
+  width: 100%;
+  text-align: left;
+  line-height: 1;
+}
+.area_participant{
+  padding: 0 0 15px;
+  border-bottom: 3px solid #333;
+  .list_participant{
+    text-align: left;
+    font-weight: 400;
+  }
+}
+.img_box{
+  width : 100%;
+  height : 0;
+  padding-top : calc(414 / 576 * 100%);
+  background: url(@/assets/image/img_santaclaus.png)center center / cover no-repeat;
+  .txt_matching{
+    display: block;
+    font-size: 50px;
+    margin-top: -23%;
+  }
+}
+
+</style>
